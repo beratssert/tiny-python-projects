7573df866e8e205f8a4273133864587377c0be35
Berat Sert <registrationbs@tutanota.com>
2024-01-24 20:58:38 +0300

    project01

---
+++ hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Purpose: Say hello
+"""
+
+import argparse
+
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    print(f"Hello, {args.name}!")
+
+
+if __name__ == '__main__':
+    main()
